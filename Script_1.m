f107d7090b048bb5e4724182475a1a5e2ebb6399
AmrAbdeen <amr.abdeen2050@gmail.com>
2018-12-10 13:03:19 +0200

    MatlabInitCommit

---
+++ Script_1.m
@@ -0,0 +1,45 @@
+% % %Script Author @ Amr Abdeen%
+% % 
+% % close all
+% % clc
+% % clear
+% % 
+%  X=[1,2,3;4,5,6]; %Test Array%
+%  plot(X)
+% % X=[1 2 3 ; 4 5 6 ; 7 8 9]
+% % %plot(X,'r*')
+% % %grid on
+% % X=linspace(1,200,50)
+% % m=1
+% % b=0
+% % Y=(X*m)+b
+% figure
+% % %hold on
+% % plot(X,Y,'*')
+% % axis([0 50 0 100])
+% % title('Plotting')
+% % xlabel('X-Axis')
+% % ylabel('Y-Axis')
+% m=[24 24 26 26]
+% n=[15 5 5 15]
+% plot(m,n,'r')
+% fill(m,n,[0.7 0.8 0.2])
+% th=0:pi/5:2*pi;
+% r=15;
+% y=(r*sin(th))+50;
+% x=(r*cos(th))+50;
+% subplot(2,2,2)
+% plot(x,y)
+% fill(x,y,[0.7 0.8 0.5])
+% axis([0 90 0 90])
+% gtext('HERE')
+% syms x %x is a symbol will be resolved later%
+% y=2*x+3;
+% subplot(2,2,1)
+% ezplot(y,[1 10])
+% title('Symbol Equation')
+%for loop in Matlab 
+for i=1:1:10
+ 
+ end
+
